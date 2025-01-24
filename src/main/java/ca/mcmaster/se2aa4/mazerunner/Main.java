f6dc97f3e4de923b5ec89c2b57b8e2c60bcf454b
tommy-shc <tommywei0511@gmail.com>
2025-01-24 16:15:06 -0500

    Implemented forward path finding

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -24,10 +24,12 @@ public class Main {
 
         CommandLineParser parser = new DefaultParser();
 
+        int[][] maze = new int[5][5];
+
         try {
 
 
-            String filename = "./examples/tiny.maz.txt";
+            String filename = "./examples/straight.maz.txt";
             CommandLine cmd = parser.parse(options, args);
 
             if(cmd.hasOption("i")){
@@ -42,14 +44,37 @@ public class Main {
             String line;
             logger.debug("Debugging log message");
 
+            int counter = 0;
+            //int prevLineLength = 0;
+
             while ((line = reader.readLine()) != null) {
+
                 for (int i = 0; i < line.length(); i++) {
+
                     if (line.charAt(i) == '#') {
                         logger.trace("WALL ");
+                        maze[counter][i] = 0;
                     } else if (line.charAt(i) == ' ') {
                         logger.trace("PASS ");
+                        if((i == 0) || (i == 4)){
+                            maze[counter][i] = 2;
+                        }else{
+                            maze[counter][i] = 1;
+                        }
                     }
                 }
+
+                if(line.length() == 0){
+                    for(int i=0;i<5;i++){
+                        if(i == 4){
+                            maze[counter][i] = 2;
+                        }else{
+                            maze[counter][i] = 1;
+                        }
+                    }
+                }
+
+                counter++;
                 logger.info(System.lineSeparator());
             }
             reader.close();
@@ -60,8 +85,30 @@ public class Main {
         } catch(Exception e) {
             logger.error("/!\\ An error has occured /!\\");
         }
+
+        System.out.println(findPath(maze, 2, 0,""));
+
+        //Print Maze
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(maze[i][j]);
+            }
+            System.out.println();
+        }
+
+
         logger.trace("**** Computing path");
         logger.info("PATH NOT COMPUTED");
         logger.trace("** End of MazeRunner");
+    }
+
+    public static String findPath(int[][] maze,int row,int col,String path){
+
+        while(maze[row][col] != 2){
+            path+="F";
+            col += 1;
+        }
+
+        return path;
     }
 }
